f22517743dd3213136f441cece77c223a24e83d9
Karel Piwko <kpiwko@redhat.com>
2014-10-29 14:39:33 +0100

    Support of abi in AVD creator

--- src/main/groovy/org/arquillian/spacelift/gradle/android/AVDCreator.groovy
+++ src/main/groovy/org/arquillian/spacelift/gradle/android/AVDCreator.groovy
@@ -6,18 +6,29 @@ import org.arquillian.spacelift.gradle.GradleSpacelift
 class AVDCreator extends Task<Object, Void> {
 
     private String target
+    private String abi
 
     public AVDCreator target(String target) {
         this.target = target
         this
     }
 
+    public AVDCreator abi(String abi) {
+        this.abi = abi
+        this
+    }
+
     @Override
     protected Void process(Object input) throws Exception {
-        GradleSpacelift.tools("android")
+        def tool = GradleSpacelift.tools("android")
                 .parameters(["create" ,"avd", "-n", target.replaceAll("\\W", ""), "-t", target, "--force"])
                 .interaction(GradleSpacelift.ECHO_OUTPUT)
-                .execute().await()
+
+        if(abi) {
+            tool.parameters("--abi", abi)
+        }
+
+        tool.execute().await()
 
         return null
     }
