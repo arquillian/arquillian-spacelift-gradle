9b3cfb0c5bd5e6965bbccf79d0e76a77e09770bf
Tadeas Kriz <tadeas.kriz@gmail.com>
2014-11-27 11:54:07 +0100

    Add try-s to ensure 'after' and 'after suite' get called if test fails.

--- src/main/groovy/org/arquillian/spacelift/gradle/Test.groovy
+++ src/main/groovy/org/arquillian/spacelift/gradle/Test.groovy
@@ -35,40 +35,43 @@ class Test implements ValueExtractor {
         logger.info(":test:${name} before suite execution")
         beforeSuite.rehydrate(new GradleSpaceliftDelegate(), this, this).call()
 
-        // iterate through beforeTest, execute and afterTest based on data provider
-        dataProvider.rehydrate(new GradleSpaceliftDelegate(), this, this).call().each { data ->
+        // in case anything in this try block fails, we will still run the `after suite` in the finally block
+        try {
+            // iterate through beforeTest, execute and afterTest based on data provider
+            dataProvider.rehydrate(new GradleSpaceliftDelegate(), this, this).call().each { data ->
 
-            if(data==null) {
-                logger.info(":test:${name} before test execution")
-                beforeTest.rehydrate(new GradleSpaceliftDelegate(), this, this).call()
-            }
-            else {
-                logger.info(":test:${name} before test execution (${data})")
-                beforeTest.rehydrate(new GradleSpaceliftDelegate(), this, this).call(data)
-            }
+                if (data == null) {
+                    logger.info(":test:${name} before test execution")
+                    beforeTest.rehydrate(new GradleSpaceliftDelegate(), this, this).call()
+                } else {
+                    logger.info(":test:${name} before test execution (${data})")
+                    beforeTest.rehydrate(new GradleSpaceliftDelegate(), this, this).call(data)
+                }
 
-            if(data==null) {
-                logger.lifecycle(":test:${name}")
-                execute.rehydrate(new GradleSpaceliftDelegate(), this, this).call()
+                // in case anything in this try block fails, we will still run the `after test` in the finally block
+                try {
+                    if (data == null) {
+                        logger.lifecycle(":test:${name}")
+                        execute.rehydrate(new GradleSpaceliftDelegate(), this, this).call()
+                    } else {
+                        logger.lifecycle(":test:${name} (${data})")
+                        execute.rehydrate(new GradleSpaceliftDelegate(), this, this).call(data)
+                    }
+                } finally {
+                    if (data == null) {
+                        logger.info(":test:${name} after test execution")
+                        afterTest.rehydrate(new GradleSpaceliftDelegate(), this, this).call()
+                    } else {
+                        logger.info(":test:${name} after test execution (${data})")
+                        afterTest.rehydrate(new GradleSpaceliftDelegate(), this, this).call(data)
+                    }
+                }
             }
-            else {
-                logger.lifecycle(":test:${name} (${data})")
-                execute.rehydrate(new GradleSpaceliftDelegate(), this, this).call(data)
-            }
-            if(data==null) {
-                logger.info(":test:${name} after test execution")
-                afterTest.rehydrate(new GradleSpaceliftDelegate(), this, this).call()
-            }
-            else {
-                logger.info(":test:${name} after test execution (${data})")
-                afterTest.rehydrate(new GradleSpaceliftDelegate(), this, this).call(data)
-            }
+        } finally {
+            // after suite
+            logger.info(":test:${name} after suite execution")
+            afterSuite.rehydrate(new GradleSpaceliftDelegate(), this, this).call()
         }
-
-        // after suite
-        logger.info(":test:${name} after suite execution")
-        afterSuite.rehydrate(new GradleSpaceliftDelegate(), this, this).call()
-
     }
 
     def execute(arg) {                
--- src/test/groovy/org/arquillian/spacelift/gradle/TestExecutionLifecycleTest.groovy
+++ src/test/groovy/org/arquillian/spacelift/gradle/TestExecutionLifecycleTest.groovy
@@ -1,68 +1,154 @@
-package org.arquillian.spacelift.gradle;
+package org.arquillian.spacelift.gradle
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.ExpectedException
+
+import static org.junit.Assert.fail
 
 class TestExecutionLifecycleTest {
 
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
     @Test
-    public void dataProviderTest() {
-        Project project = ProjectBuilder.builder().build()
-
-        project.apply plugin: 'spacelift'
-
-        project.spacelift {
-            tests {
-                bar {
-                    dataProvider { ["first", "second"] }
-                    beforeSuite { println "Executing beforeSuite" }
-                    beforeTest { value -> println "Executing beforeTest with data ${value}" }
-                    execute { value -> println "Executing test with data ${value}" }
-                    afterTest { value -> println "Executing afterTest with data ${value}" }
-                    afterSuite { println "Executing afterSuite" }
-                }
-            }
-            tools {
-            }
-            profiles {
-            }
-            installations {
-            }
-        }
-
-        GradleSpacelift.currentProject(project)
-
-        project.spacelift.tests.each { test ->
-            test.executeTest(project.logger)
+    void "all phases should execute twice when dataProvider returns an array with two items"() {
+        runAsSpaceliftTest {
+            dataProvider { ["first", "second"] }
+            beforeSuite { println "Executing beforeSuite" }
+            beforeTest { value -> println "Executing beforeTest with data ${value}" }
+            execute { value -> println "Executing test with data ${value}" }
+            afterTest { value -> println "Executing afterTest with data ${value}" }
+            afterSuite { println "Executing afterSuite" }
         }
     }
 
     @Test
-    public void noDataProviderTest() {
+    void "all phases should execute once when no dataProvider is specified"() {
+        runAsSpaceliftTest {
+            beforeSuite { println "Executing beforeSuite" }
+            beforeTest { println "Executing beforeTest" }
+            execute { println "Executing test" }
+            afterTest { println "Executing afterTest" }
+            afterSuite { println "Executing afterSuite" }
+        }
+    }
+
+    @Test
+    void "afterTest should be invoked when exception occurs in test"() {
+        runAsSpaceliftTest {
+            execute {
+                throw new IllegalStateException("Random test error.")
+            }
+            afterTest {
+                project.exception.expect(IllegalStateException)
+                project.exception.expectMessage("Random test error.")
+            }
+        }
+    }
+
+    @Test
+    void "afterSuite should be invoked when exception occurs in dataProvider"() {
+        runAsSpaceliftTest {
+            dataProvider {
+                throw new IllegalStateException("Random test error.")
+            }
+            afterSuite {
+                project.exception.expect(IllegalStateException)
+                project.exception.expectMessage("Random test error.")
+            }
+        }
+    }
+
+    @Test
+    void "afterSuite should be invoked when exception occurs in beforeTest"() {
+        runAsSpaceliftTest {
+            beforeTest {
+                throw new IllegalStateException("Random test error.")
+            }
+            afterSuite {
+                project.exception.expect(IllegalStateException)
+                project.exception.expectMessage("Random test error.")
+            }
+        }
+    }
+
+    @Test
+    void "afterSuite should be invoked when exception occurs in test"() {
+        runAsSpaceliftTest {
+            execute {
+                throw new IllegalStateException("Random test error.")
+            }
+            afterSuite {
+                project.exception.expect(IllegalStateException)
+                project.exception.expectMessage("Random test error.")
+            }
+        }
+    }
+
+    @Test
+    void "afterSuite should be invoked when exception occurs in afterTest"() {
+        runAsSpaceliftTest {
+            afterTest {
+                throw new IllegalStateException("Random test error.")
+            }
+            afterSuite {
+                project.exception.expect(IllegalStateException)
+                project.exception.expectMessage("Random test error.")
+            }
+        }
+    }
+
+    @Test
+    void "test and afterSuite should not execute when beforeSuite fails"() {
+        runAsSpaceliftTest {
+            beforeSuite {
+                project.exception.expect(IllegalStateException)
+                throw new IllegalStateException("Random before suite error.")
+            }
+            execute {
+                fail("Test should not be run.")
+            }
+            afterSuite {
+                fail("AfterSuite should not be run.")
+            }
+        }
+    }
+
+    @Test
+    void "test and afterTest should not execute when beforeTest fails"() {
+        runAsSpaceliftTest {
+            beforeTest {
+                project.exception.expect(IllegalStateException)
+                throw new IllegalStateException("Random before suite error.")
+            }
+            execute {
+                fail("Test should not be run.")
+            }
+            afterTest {
+                fail("AfterTest should not be run.")
+            }
+        }
+    }
+
+    void runAsSpaceliftTest(Closure test) {
+        runAsSpaceliftScript {
+            tests {
+                foo test
+            }
+        }
+    }
+
+    void runAsSpaceliftScript(Closure buildScript) {
         Project project = ProjectBuilder.builder().build()
 
         project.apply plugin: 'spacelift'
 
-        project.spacelift {
-            tests {
-                bar {
-                    beforeSuite { println "Executing beforeSuite" }
-                    beforeTest {  println "Executing beforeTest" }
-                    execute {  println "Executing test" }
-                    afterTest {  println "Executing afterTest" }
-                    afterSuite { println "Executing afterSuite" }
-                }
-            }
-            tools {
-            }
-            profiles {
-            }
-            installations {
-            }
-        }
+        project.spacelift buildScript
+
+        project.ext.exception = exception
 
         GradleSpacelift.currentProject(project)
 
